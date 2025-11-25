d3a518a1643793fc760f1121c1783ac8a3275d60
Max360 <blythe2106@gnail.com>
2025-11-25 15:32:05 +0800

    Added Point Class and uses.

---
+++ Point.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassCatacombs;
+
+public class Point
+{
+    public int X  { get; set; }
+    public int Y { get; set; }
+    public Point()
+    {
+        X = 0;
+        Y = 0;
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ClassCatacombs;
+
+Point myPoint = new Point();
+Point myPoint2 = new Point();
+
+myPoint.X = 2;
+myPoint.Y = 3;
+
+myPoint2.X = -4;
+myPoint2.Y = 0;
+
+Console.WriteLine($"{myPoint.X}, {myPoint.Y}");
+Console.WriteLine($"{myPoint2.X}, {myPoint2.Y}");
